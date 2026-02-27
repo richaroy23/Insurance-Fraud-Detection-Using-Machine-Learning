94623e90dd9211e277f6c603dd0551064fdcbf88
Richa Roy <richaroyghy@gmail.com>
2026-02-27 06:09:20 +0000

    handling outliers

--- eda.py
+++ eda.py
@@ -20,3 +20,53 @@ df = pd.read_csv("insurance_claims.csv")
 df.head()
 df.isna().any()
 df.isna().sum()
+
+# Visualizing Outliers with Boxplot
+sns.boxplot(data=df, x='policy_annual_premium')
+plt.title("Boxplot of Policy Annual Premium")
+plt.show()
+
+# Calculating IQR and Bounds
+Q1 = df['policy_annual_premium'].quantile(0.25)
+Q3 = df['policy_annual_premium'].quantile(0.75)
+
+IQR = Q3 - Q1
+
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+print("Lower Bound:", lower_bound)
+print("Upper Bound:", upper_bound)
+
+# Function to Visualize Distribution Before & After Transformation
+def plot_distribution(feature):
+    
+    # Original distribution
+    plt.figure(figsize=(12,5))
+    
+    plt.subplot(1,2,1)
+    sns.histplot(df[feature], kde=True)
+    plt.title("Original Distribution")
+    
+    plt.subplot(1,2,2)
+    stats.probplot(df[feature], dist="norm", plot=plt)
+    plt.title("Original Probability Plot")
+    
+    plt.show()
+    
+    # Log transformed distribution
+    df[feature + "_log"] = np.log1p(df[feature])
+    
+    plt.figure(figsize=(12,5))
+    
+    plt.subplot(1,2,1)
+    sns.histplot(df[feature + "_log"], kde=True)
+    plt.title("Log Transformed Distribution")
+    
+    plt.subplot(1,2,2)
+    stats.probplot(df[feature + "_log"], dist="norm", plot=plt)
+    plt.title("Log Transformed Probability Plot")
+    
+    plt.show()
+# Apply Transformation
+    plot_distribution('policy_annual_premium')
