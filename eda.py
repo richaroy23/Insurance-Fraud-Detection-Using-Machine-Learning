624209fe5bb146cda6e37531f565bba9c95aae4b
Richa Roy <richaroyghy@gmail.com>
2026-02-27 05:49:21 +0000

    Fix eda imports and add requirements

---
+++ eda.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report, confusion_matrix
+import warnings
+import pickle
+from scipy import stats
+warnings.filterwarnings ('ignore')
+plt.style.use('fivethirtyeight')
